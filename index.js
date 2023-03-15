c8b07b00b5d79dc3aaebb8a8b7be82f8b01667a0
alvinkenyagah <alvin47k@gmail.com>
2023-03-15 17:53:26 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,80 @@
 // Code your solution in this file!
+const drivers = ['Antonia', 'Nuru', 'Amari', 'Mo'];
+
+
+
+function returnFirstTwoDrivers(){
+
+    const twoDrivers = drivers.slice(0, 2);
+
+return twoDrivers;
+
+}
+
+
+function returnLastTwoDrivers(){
+
+const lastTwo = drivers.slice(2)
+return lastTwo;
+
+
+
+}
+
+// function selectingDrivers(drivers) {
+
+//     function returnFirstTwoDrivers(drivers) {
+//       return drivers.slice(0, 2);
+//     }
+  
+  
+//     return [returnFirstTwoDrivers, returnLastTwoDrivers];
+//   }
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+
+  function createFareMultiplier(multiplier) {
+
+    function fareMultiplier(fare) {
+      return fare * multiplier;
+    }
+  
+    return fareMultiplier;
+  }
+  
+
+  
+
+function fareDoubler(fare) {
+    return fare * 2;
+  }
+
+
+function fareTripler(fare){
+
+return fare *3;
+
+}
+
+function selectDifferentDrivers(drivers, driverSelector) {
+    return driverSelector(drivers);
+  }
+  
+  function returnFirstTwoDrivers(drivers) {
+    return drivers.slice(0, 2);
+  }
+  
+  function returnLastTwoDrivers(drivers) {
+    return drivers.slice(2);
+  }
+  
+  
+  
+  function selectDifferentDrivers(arrayOfDrivers, driverSelector) {
+    return driverSelector(arrayOfDrivers);
+  }
+  
+
+
+
